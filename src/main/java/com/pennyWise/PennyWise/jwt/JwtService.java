9aade2ad9c32c036beaa5accd63f49a29febe175
Akanshag11 <akanshagupta3006@gmail.com>
2025-07-10 18:53:48 +0530

    jwt services

---
+++ src/main/java/com/pennyWise/PennyWise/jwt/JwtService.java
@@ -0,0 +1,57 @@
+package com.pennyWise.PennyWise.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.Valid;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.logging.structured.GraylogExtendedLogFormatProperties;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    public String secret;
+
+    private Key key;
+
+    @PostConstruct
+    public void init(){
+        this.key=Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+864000000))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractEmail(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+}
